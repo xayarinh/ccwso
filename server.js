eaf96f5efc5c7687051949aa7c6904b95b49b567
Justin Xayarinh <justinxayarinh@Justins-MacBook-Pro.local>
2018-12-04 21:32:38 -0600

    fourth commit

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+//
+//
+// app.get('/', function(req, res){
+//     res.sendFile('/index.html/');
+//
+//     app.listen(4000, function(){
+//         console.log('Your node js server is running');
+//     });
+//
+// });
 
 
-app.get('/', function(req, res){
-    res.sendFile('/index.html/');
+var express = require('express');
+var app = express();
 
-    app.listen(4000, function(){
-        console.log('Your node js server is running');
-    });
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
+app.get('/', function(request, response) {
+    response.send('Hello World!');
 });
+
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'));
+});
+
