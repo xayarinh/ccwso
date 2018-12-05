25c5c1936388826e302fe6bedb6fa05bfdba43e4
Justin Xayarinh <justinxayarinh@Justins-MacBook-Pro.local>
2018-12-04 22:17:05 -0600

    added code to return index.html

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-// Load express app
-const express = require('express');
-const app = express();
+try {
+    // Load express app
+    const express = require('express');
+    const path = require('path');
+    const app = express();
 
-app.set('port', (process.env.PORT || 5000)); // set the port
-//app.use(express.static(__dirname + '/public'));
+    app.set('port', (process.env.PORT || 5000)); // set the port
 
-app.get('/', function(request, response) {
-    response.send('Hello World!');
-});
+    app.get('/', function (request, response) {
+        response.sendFile(path.join(__dirname + '/index.html'));
+    });
 
-app.listen(app.get('port'), function() {
-    console.log("Node app is running at localhost:" + app.get('port'));
-});
+    app.listen(app.get('port'), function () {
+        console.log("Node app is running at localhost:" + app.get('port'));
+    });
+} catch(error){
+    console.log('error', error);
+}
 
