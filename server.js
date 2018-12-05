f2e495e1ff4077ed82d0cd954dd655047d16d6ec
Justin Xayarinh <justinxayarinh@Justins-MacBook-Pro.local>
2018-12-04 21:51:33 -0600

    comment out .use

--- server.js
+++ server.js
@@ -1,23 +1,9 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-//
-//
-// app.get('/', function(req, res){
-//     res.sendFile('/index.html/');
-//
-//     app.listen(4000, function(){
-//         console.log('Your node js server is running');
-//     });
-//
-// });
+// Load express app
+const express = require('express');
+const app = express();
 
-
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.set('port', (process.env.PORT || 5000)); // set the port
+//app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
     response.send('Hello World!');
