de499c43db572e8af65695fc02cee9c0ef77de82
Justin Xayarinh <justinxayarinh@Justins-MacBook-Pro.local>
2018-12-08 12:57:39 -0600

    fix

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -12875,7 +12875,8 @@ module.exports = function spread(callback) {
 
 const APP_PORT = 5000;
 const DB_URL = 'mongodb://xayarinh2108:Jomamma101@ds129484.mlab.com:29484/ccwso';
-const GET_SEAT_URL = 'http://localhost:5000/api/getSeats/';
+// const GET_SEAT_URL = 'http://localhost:5000/api/getSeats/';
+const GET_SEAT_URL = 'https://ccwso.herokuapp.com/api/getSeats/';
 module.exports = { APP_PORT, DB_URL, GET_SEAT_URL };
 
 /***/ }),
