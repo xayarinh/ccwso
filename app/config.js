a081629cd71f285bfe45f11c4a4933f8619810e4
Justin Xayarinh <justinxayarinh@Justins-MacBook-Pro.local>
2018-12-11 21:12:23 -0600

    push with config

--- app/config.js
+++ app/config.js
@@ -1,5 +1,6 @@
 //const dotenv = require('dotenv').config();
 let GET_SEAT_URL = 'http://localhost:5000/api/getSeats/';
+console.log('proces',process.env.NODE_ENV);
 if(process.env.NODE_ENV === 'production'){
    GET_SEAT_URL = 'https://ccwso.herokuapp.com/api/getSeats/';
 }
--- public/build/bundle.js
+++ public/build/bundle.js
@@ -12877,6 +12877,7 @@ module.exports = function spread(callback) {
 
 /* WEBPACK VAR INJECTION */(function(process) {//const dotenv = require('dotenv').config();
 let GET_SEAT_URL = 'http://localhost:5000/api/getSeats/';
+console.log('proces', process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
    GET_SEAT_URL = 'https://ccwso.herokuapp.com/api/getSeats/';
 }
