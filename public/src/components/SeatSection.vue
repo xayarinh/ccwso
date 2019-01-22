dc22fbbe87c1cd2f4f736795d3d3dae27153b951
Justin Xayarinh <justinxayarinh@Justins-MacBook-Pro.local>
2019-01-22 15:48:27 -0600

    Commit 1/22

--- public/src/components/SeatSection.vue
+++ public/src/components/SeatSection.vue
@@ -52,8 +52,8 @@
             getSeats() {
                 // have to make sure we use the this of the vue module because it's different when it gets to the call back in axios.get
                 let self = this;
-                let url = process.env.GET_SEAT_URL;
-                // let url = 'http://ccwso.herokuapp.com/api/getSeats/';
+                // let url = process.env.GET_SEAT_URL;
+                let url = 'http://ccwso.herokuapp.com/api/getSeats/';
                 let params = {
                     params: {
                         section_data: this.section_data
